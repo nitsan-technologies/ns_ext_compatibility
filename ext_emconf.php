957b4c95058f297cf321b5224670d2d06c5e1f95
NITSAN Technologies <sanjay@nitsan.in>
2018-08-27 19:01:50 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.1.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5-8.7.99',
+			'typo3' => '4.5.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
