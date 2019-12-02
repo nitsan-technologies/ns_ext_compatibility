8d3097bd0fd6bcc859036cb5db74fbd18f73dd06
NITSAN <sanjay@nitsan.in>
2019-12-02 15:40:04 +0530

    - [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints - [TODO] Updating documentation - [TODO] Added demo link - [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => '[NITSAN] TYPO3 Extensions Compatibility Report',
-	'description' => 'One of the only TYPO3 extension which provides all the features like TYPO3 Extensions Compatibility Report, Notification of new update via Email, System overview report, Compatible with all major version of TYPO3 from 4.x to 9.x. This extension will help you to check TYPO3 upgrade technical feasibility & complexity. You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-extensions-compatibility-report-pro/',
+	'description' => 'One of the only TYPO3 extension which provides all the features like TYPO3 Extensions Compatibility Report, Notification of new update via Email, System overview report, Compatible with all major version of TYPO3 from 4.x to 9.x. This extension will help you to check TYPO3 upgrade technical feasibility & complexity. Demo: https://demo.t3terminal.com/t3t-extensions/extension-compatibility/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-extensions-compatibility-report-pro/',
 	'category' => 'module',
 	'author' => 'T3:Keval Pandya, T3:Bhavin Barad, QA:Siddharth Sheth, QA:Vandna Kalivada',
 	'author_email' => 'sanjay@nitsan.in',
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '4.0.1',
+	'version' => '4.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-9.5.99',
