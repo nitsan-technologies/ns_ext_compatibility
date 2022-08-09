1f96f63cb88b118965fec956887bcd019b95393f
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 12:52:38 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] TYPO3 Extensions Compatibility Report',
-    'description' => 'One of the only TYPO3 extension which provides all the features like TYPO3 Extensions Compatibility Report, Notification of new update via Email, System overview report, Compatible with all major version of TYPO3 from 4.x to 9.x. This extension will help you to check TYPO3 upgrade technical feasibility & complexity. Demo: https://demo.t3terminal.com/t3t-extensions/extension-compatibility/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-extensions-compatibility-report-pro/',
+    'description' => 'One of the only TYPO3 extension which provides all the features like TYPO3 Extensions Compatibility Report, Notification of new update via Email, System overview report, Compatible with all major version of TYPO3 from 4.x to 9.x. This extension will help you to check TYPO3 upgrade technical feasibility & complexity. Demo: https://demo.t3terminal.com/t3t-extensions/extension-compatibility/ You can download PRO version for more-features & free-support at https://t3planet.com/typo3-upgrade-extension-compatibility',
     'category' => 'module',
     'author' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
