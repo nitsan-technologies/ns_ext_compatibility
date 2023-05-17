7ffb319598fcef29270d05bce3f0570fe5586c3f
Divya Goklani <divya@nitsantech.com>
2023-05-17 19:57:55 +0530

    [TASK] Make js related changes and modify some templating

--- Resources/Public/JavaScript/Main.js
+++ Resources/Public/JavaScript/Main.js
@@ -1,14 +1,13 @@
 define([
     'jquery',
-    'TYPO3/CMS/Backend/Modal',
-    'TYPO3/CMS/NsExtCompatibility/Datatables',
-    'TYPO3/CMS/Backend/jquery.clearable'
-], function ($, Model) {
+    'TYPO3/CMS/NsExtCompatibility/Datatables'
+], function ($) {
  
     var mainDataTable = $('#typo3-extension-list').DataTable({
         paging: false
     });
     $('.dataTables_wrapper .dataTables_filter input').addClass("form-control");
+    $('.dataTables_wrapper .dataTables_filter label').attr('style', 'display: flex');
     // $('.dataTables_wrapper .dataTables_filter input').clearable({
     //     onClear: function () {
     //         mainDataTable.fnFilter('');
@@ -28,16 +27,16 @@ define([
             cache: false,
             success: function (data) {
                 modal.find('.modal-content').html(data);
-                if($("#extensionAllVersionlist").length>0){
-                    var dataTable = $('#extensionAllVersionlist').DataTable({
-                        paging: false
-                    });
-                    $('#nsModel .dataTables_wrapper .dataTables_filter input').clearable({
-                        onClear: function () {
-                            datatable.fnFilter('');
-                        }
-                    });
-                }
+                // if($("#extensionAllVersionlist").length>0){
+                //     var dataTable = $('#extensionAllVersionlist').DataTable({
+                //         paging: false
+                //     });
+                //     $('#nsModel .dataTables_wrapper .dataTables_filter input').clearable({
+                //         onClear: function () {
+                //             datatable.fnFilter('');
+                //         }
+                //     });
+                // }
             }
         });
     });
