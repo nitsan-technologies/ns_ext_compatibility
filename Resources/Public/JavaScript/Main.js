69ecc91748129f1a9bf8cd1bc86893333b8fe2ac
Saloni Thakkar <saloni.nitsan@gmail.com>
2026-02-05 11:21:59 +0530

    [TASK]V14 compitibility

--- Resources/Public/JavaScript/Main.js
+++ Resources/Public/JavaScript/Main.js
@@ -1,53 +1,53 @@
-define([
-    'jquery',
-    'TYPO3/CMS/Backend/Modal',
-    'TYPO3/CMS/NsExtCompatibility/Datatables'
-], function ($, Model) {
+import $ from "jquery";
+// import "@nitsan/ns-ext-compatibility/Datatables.js";
 
+
+$(document).ready(function() {
     const dropdown = document.getElementById("targetVersion");
-    dropdown.addEventListener("change", function() {
-        document.getElementById("changeTargtVersion").submit();
-    });
+    if (dropdown) {
+        dropdown.addEventListener("change", function() {
+            document.getElementById("changeTargtVersion").submit();
+        });
+    }
 
-    var mainDataTable = $('#typo3-extension-list').DataTable({
-        paging: false
-    });
-    $('.dataTables_wrapper .dataTables_filter input').addClass("form-control");
-
-    var url;
-
-    $('.ext-overview').click(function(){
-        url = $(this).attr('data-uri');
+    $(document).on('click', '.ext-overview, .viewAllVersionLink', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        
+        var url = $(this).attr('data-uri') || $(this).data('uri');
         var modal = $('#nsModel');
+        
+        if (!url) {
+            console.error('No data-uri found');
+            return false;
+        }
+        
+        // Show loading state
+        modal.find('.modal-content').html('<div class="modal-body"><p>Loading...</p></div>');
+        
         $.ajax({
             type: "GET",
             url: url,
             cache: false,
             success: function (data) {
                 modal.find('.modal-content').html(data);
+            },
+            error: function(xhr, status, error) {
+                console.error('AJAX Error:', status, error);
+                modal.find('.modal-content').html(
+                    '<div class="modal-body"><div class="alert alert-danger">Error loading content. Please try again.</div></div>'
+                );
             }
         });
+        
+        return false;
     });
 
-    $('.viewAllVersionLink').click(function(){
-        url = $(this).attr('data-uri');
-        var modal = $('#nsModel');
-        $.ajax({
-            type: "GET",
-            url: url,
-            cache: false,
-            success: function (data) {
-                modal.find('.modal-content').html(data);
-            }
-        });
+    $('#targetVersion').on('change', function() {
+        $('.ext-wrapper').show();
     });
 
-    $('#targetVersion').on('change',function (){
-       $('.ext-wrapper').show();
+    $('.tx-ext-compatibility .alert .close').on('click', function() {
+        $(this).parent().hide();
     });
-
-    $('.tx-ext-compatibility .alert .close').on('click',function (){
-        $(this).parent().hide()
-    });
-
 });
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,15 @@
 <?php
 
 $EM_CONF['ns_ext_compatibility'] = [
-    'title' => 'TYPO3 Extension Upgrade Checker',
-    'description' => 'Analyze the compatibility of your TYPO3 extensions before upgrading. Get insights, reports, and recommendations to ensure smooth version upgrades and better system stability.',
+    'title' => 'Upgrade Extensions Compatibility',
+    'description' => 'Are you in need of a TYPO3 Extension that offers features such as system information reporting, statistical analysis of TYPO3 extensions, downloadable compatibility options, and more? TYPO3 Extensions Compatibility Report, tailored to meet your specific requirements! Compatible with major versions of TYPO3 12.x, it assists you in assessing the feasibility and complexity of TYPO3 upgrades. 
     
+    ***  Live-Demo: https://demo.t3planet.com/t3-extensions/ext-compatibility *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-upgrade-extension-compatibility/',
+
     'category' => 'module',
-    'author' => 'Team T3Planet',
-    'author_email' => 'info@t3planet.de',
-    'author_company' => 'T3Planet',
+    'author' => 'T3Planet // NITSAN',
+    'author_email' => 'sanjay@nitsan.in',
+    'author_company' => 'T3Planet // NITSAN',
     'shy' => '',
     'priority' => '',
     'module' => '',
@@ -18,10 +20,11 @@ $EM_CONF['ns_ext_compatibility'] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '12.1.0',
+    'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.9.99',
+           'typo3' => '12.0.0-13.9.99',
+           'ns_license' => '13.0.0-13.9.99',
         ],
         'conflicts' => [
         ],
